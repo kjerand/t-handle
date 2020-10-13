9178b95eef63888f54ff3e9ddec72b6fbeb8e306
Dilawar Mahmood <dilawarmm@outlook.com>
2020-10-13 15:07:44 +0200

    punktgenerering med pickle

--- kode/oppgave5/index.py
+++ kode/oppgave5/index.py
@@ -11,10 +11,7 @@ from oppgave4.RK45 import RK45
 from utils.utils import get_h
 
 
-def oppgave(omega_0, n, interval):
-
-    X_0 = np.identity(3, dtype=np.double)
-
+def oppgave(X_0, omega_0, n, interval):
     L_vector = calculate_L(X_0, omega_0)
 
     I = treghetsmoment(M, R, L)
@@ -29,35 +26,75 @@ def calculate_L(X, omega):
 def draw(W, ax):
     ax.clear()
     size = 1.0
-    ax.scatter([1.0 * size, 1.0 * size, 1.0 * size, 1.0 * size, -1.0 * size, -1.0 * size, -1.0 * size, -1.0 * size], [1.0 * size, -1.0 * size, 1.0 * size, -1.0 * size, 1.0 * size, -1.0 * size, 1.0 * size, -1.0 * size], [-1.0 * size, -1.0 * size, 1.0 * size, 1.0 * size, -1.0 * size, -1.0 * size, 1.0 * size, 1.0 * size], s = 1)
-    ax.scatter([i[0] for i in W.T],[i[1] for i in W.T], [i[2] for i in W.T])
+    ax.scatter(
+        [
+            1.0 * size,
+            1.0 * size,
+            1.0 * size,
+            1.0 * size,
+            -1.0 * size,
+            -1.0 * size,
+            -1.0 * size,
+            -1.0 * size,
+        ],
+        [
+            1.0 * size,
+            -1.0 * size,
+            1.0 * size,
+            -1.0 * size,
+            1.0 * size,
+            -1.0 * size,
+            1.0 * size,
+            -1.0 * size,
+        ],
+        [
+            -1.0 * size,
+            -1.0 * size,
+            1.0 * size,
+            1.0 * size,
+            -1.0 * size,
+            -1.0 * size,
+            1.0 * size,
+            1.0 * size,
+        ],
+        s=1,
+    )
+    ax.scatter([i[0] for i in W.T], [i[1] for i in W.T], [i[2] for i in W.T])
 
 
 if __name__ == "__main__":
     n = 5000
     interval = [0.0, 2000.0]
+    X_0 = np.identity(3, dtype=np.double)
 
     omega_0_a = np.array([[1, 0.05, 0]], dtype=np.double).T
-    W_a, t, E = oppgave(omega_0_a, n, interval)
+    W_a, t, E = oppgave(X_0, omega_0_a, n, interval)
 
     omega_0_b = np.array([[0, 1, 0.05]], dtype=np.double).T
-    W_b, t, E = oppgave(omega_0_b, n, interval)
+    W_b, t, E = oppgave(X_0, omega_0_b, n, interval)
 
     omega_0_c = np.array([[0.05, 0.0, 1.0]], dtype=np.double).T
-    W_c, t, E = oppgave(omega_0_c, n, interval)
+    W_c, t, E = oppgave(X_0, omega_0_c, n, interval)
 
-<<<<<<< HEAD
-    print(W_a[n - 1].shape)
-=======
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
 
     axTime = plt.axes([0.2, 0.1, 0.65, 0.03])
-    sTime = Slider(axTime, 'Time', interval[0], interval[1], valinit=interval[0], valstep=float((interval[1] - interval[0]) / n))
+    sTime = Slider(
+        axTime,
+        "Time",
+        interval[0],
+        interval[1],
+        valinit=interval[0],
+        valstep=float((interval[1] - interval[0]) / n),
+    )
 
-    sTime.on_changed(lambda val: draw(W_c[int(((val/(interval[1] - interval[0])) * n) - interval[0])], ax))
+    sTime.on_changed(
+        lambda val: draw(
+            W_c[int(((val / (interval[1] - interval[0])) * n) - interval[0])], ax
+        )
+    )
 
-    ax.scatter([i[0] for i in W_c[0]],[i[1] for i in W_c[0]], [i[2] for i in W_c[0]])
+    ax.scatter([i[0] for i in W_c[0]], [i[1] for i in W_c[0]], [i[2] for i in W_c[0]])
 
     plt.show()
->>>>>>> 5c53a48003233c9f47ba8eaa62777037905ee90f
--- kode/oppgave6/punktgenerering.py
+++ kode/oppgave6/punktgenerering.py
@@ -3,11 +3,33 @@ import sys
 sys.path.append("..")
 
 from oppgave5.index import oppgave
+from utils.utils import get_h
+
+import numpy as np
+import pickle
+
+
+def save_data(X_0, omega_0, n, interval, filename):
+    W_c, t, E = oppgave(X_0, omega_0, n, interval)
+    pickle.dump([W_c, t, E], open(f"data/{filename}", "wb"))
+
+
+def load_data(filename):
+    data = pickle.load(open(f"data/{filename}", "rb"))
+    return (
+        np.array(data[0], dtype=np.double),
+        np.array(data[1], dtype=np.double),
+        np.array(data[2], dtype=np.double),
+    )
+
 
 if __name__ == "__main__":
-    f = open("verdier")
-    for i in range(2000):
-        verdier = oppgave([i dont know what to put here yet], i)
-        f.write(verdier)
+    n = 5000
+    interval = [0.0, 2000.0]
+    X_0 = np.identity(3, dtype=np.double)
 
-        
+    omega_0_a = np.array([[1, 0.05, 0]], dtype=np.double).T
+    save_data(X_0, omega_0_a, n, interval, "test.npy")
+
+    W, t, E = load_data("test.npy")
+    print(W.shape)
